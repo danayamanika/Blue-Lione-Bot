d8136d4cceb67dce90128f46ee293a69b6a118df
danayamanika <111094941+danayamanika@users.noreply.github.com>
2022-09-21 21:43:00 +0530

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -178,7 +178,7 @@ Time ⏰ =  ${xtime} `
 let buttonMessage = {
     image: { url: 'https://i.ibb.co/hXtGkzj/welcome-poster-spectrum-brush-strokes-white-background-colorful-gradient-brush-design-vector-paper-i.jpg'},
 caption: xeonbody,
-footer: `⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜`,
+footer: `☸ ᴅᴀɴᴀʏᴀ ʙᴏᴛ ☸`,
 buttons,
 headerType: 4
 }
@@ -200,12 +200,12 @@ Time ⏰ =  ${xeontime} `
 
    let buttons = [
     {buttonId: `owner`, buttonText: {displayText: '🐼 BYE 🐼'}, type: 1},
-    {buttonId: `menu`, buttonText: {displayText: '♞ MENU ♞'}, type: 1}
+    {buttonId: `menu`, buttonText: {displayText: '❦ MENU ❦'}, type: 1}
     ]
     let buttonMessage = {
         image: { url: 'https://i.ibb.co/gytytkM/images.jpg'},
     caption: xeonbody,
-    footer: `⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜`,
+    footer: `☸ ᴅᴀɴᴀʏᴀ ʙᴏᴛ ☸`,
     buttons,
     headerType: 4
     }
